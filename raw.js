cce38574ffd3ef6b1113ee6564d8b07220e572ae
quinguyen1009 <64306300+quinguyen1009@users.noreply.github.com>
2020-04-25 16:31:55 +0700

    Create raw.js

---
+++ raw.js
@@ -0,0 +1,15 @@
+<!-- /ct-wrapper -->
+  </div><!-- footer-credits -->
+
+<b:include data='blog' name='google-analytics'/>
+
+<script src='//code.jquery.com/jquery-1.9.1.js' type='text/javascript'/>
+<script src='https://raw.githack.com/linhfaceseo/cssjs/master/redirectcopy.js' type='text/javascript'/>
+<script src='https://raw.githack.com/linhfaceseo/mysystemseo/master/popupan.js'/>
+ 
+
+  <link href='https://raw.githack.com/mrtodesigner/hvbw/master/formfaceseo-2020.css' rel='stylesheet' type='text/css'/>
+  <script src='https://raw.githack.com/mrtodesigner/hvbw/master/formfaceseo-2020.js' type='text/javascript'/>
+
+<script>
+jQuery( document ).ready( function( $ ) {
